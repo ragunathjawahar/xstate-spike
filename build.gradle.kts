6ada80f1361d0bd7c0fe89826e584fc68729e9c9
Ragunath Jawahar <ragunath@redgreen.io>
2021-02-04 09:27:42 +0530

    build: add Approval Tests dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,15 @@ dependencies {
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+
+    testImplementation("com.approvaltests:approvaltests:9.5.0")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
 
