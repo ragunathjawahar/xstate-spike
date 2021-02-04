a639e64b2d9f261f8c36e3134848322b4f4e0663
Ragunath Jawahar <ragunath@redgreen.io>
2021-02-04 15:58:56 +0530

    refactor: replace hard-coded JSON keys with constant vals

--- src/main/kotlin/xstate/visitors/XStateJsonVisitor.kt
+++ src/main/kotlin/xstate/visitors/XStateJsonVisitor.kt
@@ -6,6 +6,13 @@ import xstate.DslVisitor
 import xstate.visitors.mobius.Reducer
 
 class XStateJsonVisitor : DslVisitor {
+  companion object {
+    private const val KEY_ID = "id"
+    private const val KEY_INITIAL = "initial"
+    private const val KEY_STATES = "states"
+    private const val KEY_ON = "on"
+  }
+
   private val machineJsonObject = JSONObject()
   private var currentStateJsonObject: JSONObject? = null
 
@@ -13,25 +20,25 @@ class XStateJsonVisitor : DslVisitor {
     get() = machineJsonObject.toString(2) + "\n\n~ NOTE: Link to the viz tool: https://xstate.js.org/viz/ ~"
 
   override fun onName(name: String) {
-    machineJsonObject.put("id", name) // TODO: 04/02/21 Extract constants and put them in one place
+    machineJsonObject.put(KEY_ID, name) // TODO: 04/02/21 Extract constants and put them in one place
   }
 
   override fun onInitialState(
     initialState: KClass<out Any>,
     effects: Set<KClass<out Any>>
   ) {
-    machineJsonObject.put("initial", initialState.simpleName)
+    machineJsonObject.put(KEY_INITIAL, initialState.simpleName)
   }
 
   override fun onState(state: KClass<out Any>) {
     currentStateJsonObject = JSONObject()
 
-    if (!machineJsonObject.has("states")) {
+    if (!machineJsonObject.has(KEY_STATES)) {
       val statesJsonObject = JSONObject()
-      machineJsonObject.put("states", statesJsonObject)
+      machineJsonObject.put(KEY_STATES, statesJsonObject)
       statesJsonObject.put(state.simpleName, currentStateJsonObject)
     } else {
-      val statesJsonObject = machineJsonObject.get("states") as JSONObject
+      val statesJsonObject = machineJsonObject.get(KEY_STATES) as JSONObject
       statesJsonObject.put(state.simpleName, currentStateJsonObject)
     }
   }
@@ -42,14 +49,14 @@ class XStateJsonVisitor : DslVisitor {
     effects: Set<KClass<out Any>>,
     reducer: KClass<out Reducer<out Any, out Any>>
   ) {
-    val containsOn = currentStateJsonObject!!.has("on")
+    val containsOn = currentStateJsonObject!!.has(KEY_ON)
     if (!containsOn) {
       val onJsonObject = JSONObject().apply {
         put(event.simpleName, next.simpleName)
       }
-      currentStateJsonObject!!.put("on", onJsonObject)
+      currentStateJsonObject!!.put(KEY_ON, onJsonObject)
     } else {
-      (currentStateJsonObject!!.get("on") as JSONObject).put(event.simpleName, next.simpleName)
+      (currentStateJsonObject!!.get(KEY_ON) as JSONObject).put(event.simpleName, next.simpleName)
     }
   }
 }
