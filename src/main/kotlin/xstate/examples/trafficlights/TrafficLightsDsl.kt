5e63cf2bfc8bfcefdfbecbcaff55d0bb53d091b9
Ragunath Jawahar <ragunath@redgreen.io>
2021-02-04 13:43:53 +0530

    fix: yellow to red transition duration

--- src/main/kotlin/xstate/examples/trafficlights/TrafficLightsDsl.kt
+++ src/main/kotlin/xstate/examples/trafficlights/TrafficLightsDsl.kt
@@ -18,17 +18,17 @@ val trafficLightsStateMachine = Machine
   .create<TrafficLightsState, TrafficLightsEvent, TrafficLightsEffect>("Traffic Lights", Green::class) {
     states {
       state(Green::class) {
-        on(CountDownElapsed::class, next = Yellow::class, effects = setOf(BeginCountDown::class))
+        on(
+          CountDownElapsed::class,
+          next = Yellow::class,
+          effects = setOf(BeginCountDown::class),
+          reducer = GreenToYellowReducer::class
+        )
       }
       state(Yellow::class) {
         // Despite having a reducer, we still have to specify the state class. We also have to figure out
         // how to keep the next state, effects, and the reducer in sync.
-        on(
-          CountDownElapsed::class,
-          next = Red::class,
-          effects = setOf(BeginCountDown::class),
-          reducer = YellowToRedReducer::class
-        )
+        on(CountDownElapsed::class, next = Red::class, effects = setOf(BeginCountDown::class))
       }
       state(Red::class) {
         on(CountDownElapsed::class, next = Green::class, effects = setOf(BeginCountDown::class))
@@ -37,7 +37,7 @@ val trafficLightsStateMachine = Machine
   }
 
 // TODO: 03/02/21 Okay to have object or class as a reducer?
-class YellowToRedReducer : Reducer<Yellow, CountDownElapsed> {
-  override fun reduce(currentState: Yellow, event: CountDownElapsed): ReductionResult =
-    StateEffect(Red, BeginCountDown(YELLOW_TO_RED_DURATION))
+class GreenToYellowReducer : Reducer<Green, CountDownElapsed> {
+  override fun reduce(currentState: Green, event: CountDownElapsed): ReductionResult =
+    StateEffect(Yellow, BeginCountDown(YELLOW_TO_RED_DURATION))
 }
